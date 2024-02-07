48bc649be8307c10c6071a7b5b29882062a9cb53
Lilian Graciano <lilian.graciano123@outlook.com>
2024-02-07 19:56:23 -0300

    Subindo exercicio 3 para teste.

---
+++ EXERCICIO3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+namespace EXERCICIO3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminhodoarquivojson = "faturamento.json";
+            FaturamentoMensal faturMensal = LerFaturamentoJson(caminhodoarquivojson);
+
+            decimal menorValor = faturMensal.Faturamento.Min();
+            decimal maiorValor = faturMensal.Faturamento.Max();
+            decimal mediaMensal = faturMensal.Faturamento.Average();
+            int diasAcimaDaMedia = faturMensal.Faturamento.Count(valor => valor > mediaMensal);
+
+          
+            Console.WriteLine($"Menor valor de faturamento: {menorValor}");
+            Console.WriteLine($"Maior valor de faturamento: {maiorValor}");
+            Console.WriteLine($"Número de dias com faturamento acima da média mensal: {diasAcimaDaMedia}");
+        }
+
+        static FaturamentoMensal LerFaturamentoJson(string caminhoDoArquivo)
+        {
+            using (StreamReader r = new StreamReader(caminhoDoArquivo))
+            {
+                string json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<FaturamentoMensal>(json);
+            }
+        }
+    }
+
+    class FaturamentoMensal
+    {
+        public decimal[] Faturamento { get; set; }
+    }
+}
